fd35407aeeddc3f8f4a46114eea37058c7ddc915
Simo Nikula <simonikula@gmail.com>
2014-07-11 22:50:37 +0300

    initial constraints for specimen using springs

--- Demos/CharpyDemo/CharpyDemo.cpp
+++ Demos/CharpyDemo/CharpyDemo.cpp
@@ -19,6 +19,7 @@ Simo Nikula 2014 based on bullet3 App_FractureDemo
 
 int sFrameNumber = 0;
 bool firstRun=true;
+btScalar startAngle(1);
 
 #include "btFractureBody.h"
 #include "btFractureDynamicsWorld.h"
@@ -35,6 +36,7 @@ void	CharpyDemo::initPhysics()
 		setDebugMode(btIDebugDraw::DBG_DrawText|btIDebugDraw::DBG_NoHelpText);
 		// we look at quite small object
 		setCameraDistance(btScalar(0.5));
+		m_cameraPosition.setX(btScalar(0.3));
 		m_frustumZNear=btScalar(0.01);
 		m_frustumZFar=btScalar(10);
 		firstRun=false;
@@ -111,12 +113,15 @@ void	CharpyDemo::initPhysics()
 	// backside at x=0
 	{
 		btScalar sMass=0.01*0.01*0.0275*7800;
-		btScalar halfLength=0.01375;
+		btScalar l=0.055;
+		btScalar halfLength=l/4;
 		btCollisionShape* shape = 
 			new btBoxShape(btVector3(0.005,0.005,halfLength));
 		m_collisionShapes.push_back(shape);
 		btVector3 localInertia(0,0,0);
 		shape->calculateLocalInertia(sMass,localInertia);
+		btAlignedObjectArray<btRigidBody*> ha;
+		btAlignedObjectArray<btTransform> ta;
 		for (int i=0;i<2;i++)
 		{	
 			btTransform tr;
@@ -127,7 +132,43 @@ void	CharpyDemo::initPhysics()
 			btRigidBody::btRigidBodyConstructionInfo rbInfo(sMass,myMotionState,shape,localInertia);
 			btFractureBody* body = new btFractureBody(rbInfo, m_dynamicsWorld);
 			m_dynamicsWorld->addRigidBody(body);
+			ha.push_back(body);
+			btTransform ctr;
+			ctr.setIdentity();
+			btVector3 cpos(0,0,(i==0?1:-1)*halfLength);
+			ctr.setOrigin(cpos);
+			ta.push_back(ctr);
 		}
+		btGeneric6DofSpringConstraint *sc=
+						new btGeneric6DofSpringConstraint(*ha[0],*ha[1], 
+			ta[0], ta[1],true);
+		btScalar E(200e9);
+		btScalar fu(400e6);
+		btScalar b(0.01);
+		btScalar h(0.08);
+		btScalar I1(b*h*h*h/12);
+		btScalar I2(h*b*b*b/12);
+		btScalar k0(E*b*h/l/2);
+		btScalar k1(48*E*I1/l/l/l);
+		btScalar k2(48*E*I1/l/l/l);
+		btScalar w1(fu*b*h*h/4);
+		btScalar w2(fu*b*b*h/4);
+		sc->setStiffness(0,k0);
+		sc->setStiffness(1,k1);
+		sc->setStiffness(2,k2);
+		sc->setStiffness(3,w1); // not very exact
+		sc->setStiffness(4,w2); 
+		sc->setStiffness(5,w1); 
+		m_dynamicsWorld->addConstraint(sc, true);
+		for (int i=0;i<6;i++)
+		{	
+			sc->enableSpring(i,true);
+		}
+		for (int i=0;i<6;i++)
+		{	
+			sc->setDamping(i,btScalar(0));
+		}
+		sc->setEquilibriumPoint();
 	}
 	// hammer with arm and hinge
 	// hammer should be able to provide impact of about 500 J
@@ -153,11 +194,11 @@ void	CharpyDemo::initPhysics()
 		compound->addChildShape(aTr,arm);
 		btTransform cTr;
 		cTr.setIdentity();
-		btVector3 cPos(btScalar(0),btScalar(2),btScalar(0));
+		btVector3 cPos(btScalar(0),btScalar(1.8),btScalar(0));
 		cTr.setOrigin(cPos);
 		btQuaternion cRot;
 		btVector3 axis(btScalar(0),btScalar(0),btScalar(1));
-		cRot.setRotation(axis,btScalar(3.1));
+		cRot.setRotation(axis,btScalar(startAngle)); // pi means up
 		cTr.setRotation(cRot);
 		btRigidBody *hBody=localCreateRigidBody(hMass+aMass,cTr,compound);
 		btVector3 aDims(btScalar(0.01),btScalar(0.01),btScalar(0.05));
@@ -231,6 +272,8 @@ void CharpyDemo::showMessage()
 		btFractureDynamicsWorld* world = (btFractureDynamicsWorld*)m_dynamicsWorld;
 		yStart+=20;
 		GLDebugDrawString(xStart,yStart,"space to restart");
+		yStart+=20;
+		GLDebugDrawString(xStart,yStart,"+/- to change start angle");
 		resetPerspectiveProjection();
 		glEnable(GL_LIGHTING);
 	}
@@ -257,7 +300,24 @@ void CharpyDemo::displayCallback(void) {
 
 void CharpyDemo::keyboardUpCallback(unsigned char key, int x, int y)
 {
-	PlatformDemoApplication::keyboardUpCallback(key,x,y);
+	switch (key)
+	{
+	case '+':
+		{
+			startAngle+=0.1;
+			clientResetScene();
+			break;
+		}
+	case '-':
+		{
+			startAngle-=0.1;
+			clientResetScene();
+			break;
+		}
+	default:
+		PlatformDemoApplication::keyboardUpCallback(key,x,y);
+		break;
+	}
 }
 
 // no-op
